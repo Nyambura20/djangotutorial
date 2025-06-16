489e247ff06c0724efd1076716a32c1b90fcd501
BSIT606J2022 <116421244+BSIT606J2022@users.noreply.github.com>
2025-06-16 15:35:56 +0300

    updated

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,17 @@
-from django.db.models import F
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.db.models import F
+from .models import Question, Choice
+from django.utils import timezone
+
+# Create your views here.
+from .models import Question
 from django.views import generic
 from django.utils import timezone
 
-from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
@@ -17,7 +23,9 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+            :5
+        ]
 
 
 class DetailView(generic.DetailView):
@@ -34,9 +42,6 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
-
-    def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def vote(request, question_id):
